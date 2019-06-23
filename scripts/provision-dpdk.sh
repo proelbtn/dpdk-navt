9100097105a7aa5b66bfe9ba5f6e9e66e3a5f516
proelbtn <proelbtn@users.noreply.github.com>
2019-06-23 17:07:42 +0900

    fix the path where DPDK is installed

--- scripts/provision-dpdk.sh
+++ scripts/provision-dpdk.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-mv /tmp/authorized_keys /root/.ssh/authorized_keys
-chown root:root /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+cd /root
+
+mv /tmp/authorized_keys ./.ssh/authorized_keys
+chown root:root ./.ssh/authorized_keys
+chmod 600 ./.ssh/authorized_keys
 
 apt update
 apt install -y build-essential libnuma-dev libpcap-dev
